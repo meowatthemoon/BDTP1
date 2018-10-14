23dd87c5f813fb0e9285ebdf6c6b0c01d50c582b
gasparramoa <>
2018-10-14 20:09:38 +0100

    4ª Parte -> Log e LogController Modificações

--- src/bd_tp1/LogController.java
+++ src/bd_tp1/LogController.java
@@ -5,13 +5,23 @@
  */
 package bd_tp1;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -19,43 +29,43 @@ import javafx.scene.control.TableView;
  * @author ramoa
  */
 public class LogController implements Initializable {
-
-    @FXML
-    TableView tabelaLOG;
     
     @FXML
     Button voltar;
     
     @FXML
-    TableColumn CNumReg;
+    GridPane gridLog;
+    
+    //Ainda não sei se preciso disto:
+    int facturaID_selecionada;
+    volatile Thread timer;
     
     @FXML
-    TableColumn CEventType;
+    private void handleActioVoltar(ActionEvent event) {
+        Parent window3; //we need to load the layout that we want to swap
+        try {
+            window3 = FXMLLoader.load(getClass().getResource("Main_Menu.fxml"));
+            Scene newScene; //then we create a new scene with our new layout
+            newScene = new Scene(window3);
+            Stage mainWindow; //Here is the magic. We get the reference to main Stage.
+            mainWindow = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            mainWindow.setScene(newScene); //here we simply set the new scene
+        } catch (IOException ex) {
+        }
+    }
     
-    @FXML
-    TableColumn CObjecto;
-    
-    @FXML
-    TableColumn CValor;
-    
-    @FXML
-    TableColumn CReferencia;
-    
-    @FXML
-    TableColumn CUserID;
-    
-    @FXML
-    TableColumn CTerminaID;
-    
-    @FXML
-    TableColumn CTerminalName;
-    
-    @FXML
-    TableColumn CDCriacao;
+    public void mostrarLogOperations(){
+        
+        gridLog.getChildren().clear();
+        gridLog.add(new Label("NumReg"), 0, 0);
+
+    }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+    
     
 }
