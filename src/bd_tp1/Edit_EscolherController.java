b7bb63a6b519da638669bc465134e4a63d1fc2c2
gasparramoa <ramoawork@gmail.com>
2018-11-02 14:22:45 +0000

    Edit com table views a funcminar

--- src/bd_tp1/Edit_EscolherController.java
+++ src/bd_tp1/Edit_EscolherController.java
@@ -12,6 +12,10 @@ import java.sql.SQLException;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -23,8 +27,11 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
 /**
  * FXML Controller class
@@ -37,6 +44,12 @@ public class Edit_EscolherController implements Initializable {
     GridPane gridPane;
     @FXML 
     ChoiceBox CBIsolLevel;
+    @FXML
+    TableView TVfatura;
+    
+    private ObservableList<ObservableList> data;
+    int facturaID_selecionada;
+    private databaseConnection dbc = new databaseConnection();
 
     @FXML
     private void handleActioVoltar(ActionEvent event) {
@@ -50,6 +63,25 @@ public class Edit_EscolherController implements Initializable {
             mainWindow.setScene(newScene); //here we simply set the new scene
         } catch (IOException ex) {
         }
+    }
+    
+    @FXML
+    private void handleActionSelectionar(ActionEvent event){
+
+        
+        new editFactura(Integer.parseInt(TVfatura.getSelectionModel().getSelectedItem().toString().substring(1, TVfatura.getSelectionModel().getSelectedItem().toString().indexOf(","))));
+                        Parent window3; //we need to load the layout that we want to swap
+                        try {
+                            window3 = FXMLLoader.load(getClass().getResource("Edit.fxml"));
+                            Scene newScene; //then we create a new scene with our new layout
+                            newScene = new Scene(window3);
+                            Stage mainWindow; //Here is the magic. We get the reference to main Stage.
+                            mainWindow = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                            mainWindow.setScene(newScene); //here we simply set the new scene
+                        } catch (IOException ex) {
+                        }
+        
+      
     }
 
     @Override
@@ -66,40 +98,59 @@ public class Edit_EscolherController implements Initializable {
         CBIsolLevel.getItems().addAll((Object[]) isolLevel);
         CBIsolLevel.getSelectionModel().select(1);
         
-        ResultSet facturas = new databaseConnection().createQuery("Select * from Factura");
-        try {
-            gridPane.add(new Label("Factura ID"), 0, 0);
-            gridPane.add(new Label("Cliente ID"), 1, 0);
-            gridPane.add(new Label("Nome"), 2, 0);
-            gridPane.add(new Label("Morada"), 3, 0);
-            int index_linha = 1;
-            while (facturas.next()) {
-                Button b = new Button(facturas.getInt(1) + "");
-                b.setOnAction(new EventHandler<ActionEvent>() {
-                    @Override
-                    public void handle(ActionEvent event) {
-                        new editFactura(Integer.parseInt(b.getText()));
-                        Parent window3; //we need to load the layout that we want to swap
-                        try {
-                            window3 = FXMLLoader.load(getClass().getResource("Edit.fxml"));
-                            Scene newScene; //then we create a new scene with our new layout
-                            newScene = new Scene(window3);
-                            Stage mainWindow; //Here is the magic. We get the reference to main Stage.
-                            mainWindow = (Stage) ((Node) event.getSource()).getScene().getWindow();
-                            mainWindow.setScene(newScene); //here we simply set the new scene
-                        } catch (IOException ex) {
-                        }
-                    }
+        
+       
+        data = FXCollections.observableArrayList();
+        try{
+            
+            String SQL = "SELECT TOP(50) * FROM Factura";
+            //ResultSet
+            ResultSet rs = dbc.createQuery(SQL);
+
+            /**********************************
+             * TABLE COLUMN ADDED DYNAMICALLY *
+             **********************************/
+            for(int i=0 ; i<rs.getMetaData().getColumnCount(); i++){
+                //We are using non property style for making dynamic table
+                final int j = i;                
+                TableColumn col = new TableColumn(rs.getMetaData().getColumnName(i+1));
+                col.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<ObservableList,String>,ObservableValue<String>>(){                    
+                    public ObservableValue<String> call(TableColumn.CellDataFeatures<ObservableList, String> param) {                                                                                              
+                        return new SimpleStringProperty(param.getValue().get(j).toString());                        
+                    }                    
                 });
-                gridPane.add(b, 0, index_linha);
-                gridPane.add(new Label(facturas.getInt(2) + ""), 1, index_linha);
-                gridPane.add(new Label(facturas.getString(3)), 2, index_linha);
-                gridPane.add(new Label(facturas.getString(4)), 3, index_linha);
-                index_linha++;
+
+                TVfatura.getColumns().addAll(col); 
+                System.out.println("Column ["+i+"] ");
             }
-        } catch (SQLException ex) {
-            Logger.getLogger(Edit_EscolherController.class.getName()).log(Level.SEVERE, null, ex);
-        }
+
+            /********************************
+             * Data added to ObservableList *
+             ********************************/
+            while(rs.next()){
+                //Iterate Row
+                ObservableList<String> row = FXCollections.observableArrayList();
+                for(int i=1 ; i<=rs.getMetaData().getColumnCount(); i++){
+                    //Iterate Column
+                    row.add(rs.getString(i));
+                }
+                System.out.println("Row [1] added "+row );
+                data.add(row);
+
+            }
+
+            //FINALLY ADDED TO TableView
+            TVfatura.setItems(data);
+          }catch(Exception e){
+              e.printStackTrace();
+              System.out.println("Error on Building Data");             
+          }
+    
+        
+        
+        
+        
+       
     }
 
 }
