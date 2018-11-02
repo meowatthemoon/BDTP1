a2da81349aa78f1ea68711d18eb15179564c8e4c
HolyMichael <555migalves555@gmail.com>
2018-11-02 12:04:08 +0000

    added table view

--- src/bd_tp1/BrowserController.java
+++ src/bd_tp1/BrowserController.java
@@ -13,6 +13,10 @@ import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Platform;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -23,8 +27,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellDataFeatures;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
 /**
  * FXML Controller class
@@ -34,16 +42,18 @@ import javafx.stage.Stage;
 public class BrowserController implements Initializable {
 
     @FXML
-    GridPane gridFacturas;
+    TableView TVfatura;
     @FXML
-    GridPane gridLinhas;
+    TableView TVfactLinha;
+    
+    private databaseConnection dbc = new databaseConnection();
     int facturaID_selecionada;
     volatile Thread timer;
+    private ObservableList<ObservableList> data;
 
     @FXML
     private void handleActioRefresh(ActionEvent event) {
         mostraFacturas();
-        mostraLinhas();
     }
 
     @FXML
@@ -60,63 +70,112 @@ public class BrowserController implements Initializable {
         } catch (IOException ex) {
         }
     }
-
-    public void mostraFacturas() {
-        gridFacturas.getChildren().clear();
-        gridFacturas.add(new Label("Factura ID"), 0, 0);
-        gridFacturas.add(new Label("Cliente ID"), 1, 0);
-        gridFacturas.add(new Label("Nome"), 2, 0);
-        gridFacturas.add(new Label("Morada"), 3, 0);
-        int index_linha = 1;
-        ResultSet facturas = new databaseConnection().createQuery("Select * from Factura");
-        try {
-            while (facturas.next()) {
-                Button b = new Button(facturas.getInt("FacturaID") + "");
-                b.setOnAction(new EventHandler<ActionEvent>() {
-                    @Override
-                    public void handle(ActionEvent event) {
-                        Button temp = (Button) event.getSource();
-                        facturaID_selecionada = Integer.parseInt(temp.getText());
-                        mostraLinhas();
-                    }
-                });
-                gridFacturas.add(b, 0, index_linha);
-                gridFacturas.add(new Label(facturas.getInt("ClienteID") + ""), 1, index_linha);
-                gridFacturas.add(new Label(facturas.getString("Nome")), 2, index_linha);
-                gridFacturas.add(new Label(facturas.getString("Morada")), 3, index_linha);
-                index_linha++;
-            }
-        } catch (SQLException ex) {
-        }
+    
+    @FXML
+    private void handleActionMostrarLinhas(ActionEvent event){
+        mostraLinhas(Integer.parseInt(TVfatura.getSelectionModel().getSelectedItem().toString().substring(1,TVfatura.getSelectionModel().getSelectedItem().toString().indexOf(","))));
     }
 
-    public void mostraLinhas() {
-        gridLinhas.getChildren().clear();
-        if (facturaID_selecionada < 1) {
-            return;
-        }
-        gridLinhas.add(new Label("ProdutoID"), 0, 0);
-        gridLinhas.add(new Label("Designacao"), 1, 0);
-        gridLinhas.add(new Label("Preco"), 2, 0);
-        gridLinhas.add(new Label("Qtd"), 3, 0);
-        ResultSet linhas = new databaseConnection().createQuery("Select ProdutoID,Designacao,Preco,Qtd from FactLinha where FacturaID=" + facturaID_selecionada);
-        try {
-            int index_linha = 1;
-            while (linhas.next()) {
-                gridLinhas.add(new Label(linhas.getInt("ProdutoID") + ""), 0, index_linha);
-                gridLinhas.add(new Label(linhas.getString("Designacao")), 1, index_linha);
-                gridLinhas.add(new Label(linhas.getFloat("Preco") + ""), 2, index_linha);
-                gridLinhas.add(new Label(linhas.getInt("Qtd") + ""), 3, index_linha);
-                index_linha++;
-            }
-        } catch (SQLException ex) {
-        }
+    public void mostraFacturas() {
+        data = FXCollections.observableArrayList();
+        try{
+            
+            String SQL = "SELECT TOP(50) * FROM Factura";
+            //ResultSet
+            ResultSet rs = dbc.createQuery(SQL);
 
+            /**********************************
+             * TABLE COLUMN ADDED DYNAMICALLY *
+             **********************************/
+            for(int i=0 ; i<rs.getMetaData().getColumnCount(); i++){
+                //We are using non property style for making dynamic table
+                final int j = i;                
+                TableColumn col = new TableColumn(rs.getMetaData().getColumnName(i+1));
+                col.setCellValueFactory(new Callback<CellDataFeatures<ObservableList,String>,ObservableValue<String>>(){                    
+                    public ObservableValue<String> call(CellDataFeatures<ObservableList, String> param) {                                                                                              
+                        return new SimpleStringProperty(param.getValue().get(j).toString());                        
+                    }                    
+                });
+
+                TVfatura.getColumns().addAll(col); 
+                System.out.println("Column ["+i+"] ");
+            }
+
+            /********************************
+             * Data added to ObservableList *
+             ********************************/
+            while(rs.next()){
+                //Iterate Row
+                ObservableList<String> row = FXCollections.observableArrayList();
+                for(int i=1 ; i<=rs.getMetaData().getColumnCount(); i++){
+                    //Iterate Column
+                    row.add(rs.getString(i));
+                }
+                System.out.println("Row [1] added "+row );
+                data.add(row);
+
+            }
+
+            //FINALLY ADDED TO TableView
+            TVfatura.setItems(data);
+          }catch(Exception e){
+              e.printStackTrace();
+              System.out.println("Error on Building Data");             
+          }
+    }
+
+    public void mostraLinhas(int ID) {
+        data = FXCollections.observableArrayList();
+        try{
+            
+            String SQL = "SELECT * FROM FactLinha WHERE FacturaID=" + ID;
+            //ResultSet
+            ResultSet rs = dbc.createQuery(SQL);
+
+            /**********************************
+             * TABLE COLUMN ADDED DYNAMICALLY *
+             **********************************/
+            for(int i=0 ; i<rs.getMetaData().getColumnCount(); i++){
+                //We are using non property style for making dynamic table
+                final int j = i;                
+                TableColumn col = new TableColumn(rs.getMetaData().getColumnName(i+1));
+                col.setCellValueFactory(new Callback<CellDataFeatures<ObservableList,String>,ObservableValue<String>>(){                    
+                    public ObservableValue<String> call(CellDataFeatures<ObservableList, String> param) {                                                                                              
+                        return new SimpleStringProperty(param.getValue().get(j).toString());                        
+                    }                    
+                });
+
+                TVfactLinha.getColumns().addAll(col); 
+                System.out.println("Column ["+i+"] ");
+            }
+
+            /********************************
+             * Data added to ObservableList *
+             ********************************/
+            while(rs.next()){
+                //Iterate Row
+                ObservableList<String> row = FXCollections.observableArrayList();
+                for(int i=1 ; i<=rs.getMetaData().getColumnCount(); i++){
+                    //Iterate Column
+                    row.add(rs.getString(i));
+                }
+                System.out.println("Row [1] added "+row );
+                data.add(row);
+
+            }
+
+            //FINALLY ADDED TO TableView
+            TVfactLinha.setItems(data);
+          }catch(Exception e){
+              e.printStackTrace();
+              System.out.println("Error on Building Data");             
+          }
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        facturaID_selecionada = 0;//pk na definicao da tabela os IDS teem de ser >=1
+        mostraFacturas();
+        /*facturaID_selecionada = 0;//pk na definicao da tabela os IDS teem de ser >=1
 
         timer = new Thread() {
             public void run() {
@@ -135,7 +194,7 @@ public class BrowserController implements Initializable {
                 }
             }
         };
-        timer.start();
+        timer.start();*/
     }
 
 }
